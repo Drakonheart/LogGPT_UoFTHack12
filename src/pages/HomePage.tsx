40a45c597c2df21fb5392ab5aeb1f3cafac7ca25
arminfarhang <armfar14@gmail.com>
2025-01-18 13:58:27 -0500

    fixing library imports and bugs

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,6 +10,7 @@ const HomePage: React.FC = () => {
     <div className="home_page_main">
       <ChatHud story={story} />
       <InfoHud story={story} setStory={setStory} />
+
     </div>
   );
 };
